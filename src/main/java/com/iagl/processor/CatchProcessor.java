00afdfc82a26f9fc565d6e292b88ac436081c4b5
Pauline Dessingue <dessingue.pauline@gmail.com>
2015-11-03 20:31:43 +0100

    Init du repo

---
+++ src/main/java/com/iagl/processor/CatchProcessor.java
@@ -0,0 +1,16 @@
+package com.iagl.processor;
+
+import org.apache.log4j.Level;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtCatch;
+
+public class CatchProcessor extends AbstractProcessor<CtCatch> {
+
+  public void process(CtCatch element) {
+    if (element.getBody().getStatements().size() == 0) {
+      getFactory().getEnvironment().report(this, Level.WARN, element, "empty catch clause");
+    }
+  }
+
+}
