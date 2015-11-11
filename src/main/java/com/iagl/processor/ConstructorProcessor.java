b19871be0ad2efb63e0bda2303eb02e00e59b4d4
Pauline Dessingue <dessingue.pauline@gmail.com>
2015-11-11 23:38:35 +0100

    Replace class field + remove annotation + remove constructor parameters

--- src/main/java/com/iagl/processor/ConstructorProcessor.java
+++ src/main/java/com/iagl/processor/ConstructorProcessor.java
@@ -5,8 +5,16 @@ import java.io.PrintWriter;
 import java.util.List;
 
 import spoon.processing.AbstractManualProcessor;
+import spoon.reflect.code.CtStatement;
+import spoon.reflect.declaration.CtAnnotation;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.CtField;
+import spoon.reflect.declaration.CtParameter;
+import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.visitor.filter.TypeFilter;
+import spoon.support.reflect.declaration.CtFieldImpl;
+import spoon.support.reflect.reference.CtTypeReferenceImpl;
 import util.SaveMap;
 
 public class ConstructorProcessor extends AbstractManualProcessor {
@@ -15,78 +23,144 @@ public class ConstructorProcessor extends AbstractManualProcessor {
 
   @Override
   public void process() {
-
     PrintWriter writer;
     try {
-      //writer = new PrintWriter("C:/Users/Pauline/Desktop/ctor-process.txt");
-      writer = new PrintWriter("C:/Users/AnaGissel/Desktop/ctor.txt");
+      writer = new PrintWriter("C:/Users/Pauline/Desktop/bindmap-list.txt");
+      
+    // On recupere la liste des contructeurs
+    List<CtConstructor> constructors = getFactory().Package().getRootPackage().getElements(new TypeFilter(CtConstructor.class));
 
-      // On recupere la liste des contructeurs
-      List<CtConstructor> constructors = getFactory().Package().getRootPackage().getElements(new TypeFilter(CtConstructor.class));
+    for (CtConstructor ctor : constructors) {
+      Integer indexAnnotation = getIndexAnnotatedInjectConstructor(ctor);
 
-      for (CtConstructor ctor : constructors) {
-
-        // On ne garde que les constructeurs avec l'annotation @Inject
-        if (isAnnotatedInjectConstructor(ctor)) {
-          writer.println("Remplacement du constructeur : " + ctor.getType());
-          // Remplacement
-          remplaceConstructor(ctor);
-        }
+      // On ne garde que les constructeurs avec l'annotation @Inject
+      if (indexAnnotation != null) {
+        remplaceConstructor(ctor, indexAnnotation);
       }
-      writer.close();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
+    } 
+    writer.close();
+  } catch (FileNotFoundException e) {
+    e.printStackTrace();
+  }
   }// fin process
 
+  
   /**
-   * Retourne true si le constructeur est annote de @Inject, false sinon
-   * @param constructor le constructeur a verifier
-   * @return True s'il est annote @Inject, false sinon
+   * Returns true if the constructor have an @Inject annotation
+   * @param constructor the constructor to verify
+   * @return 
    */
-  private boolean isAnnotatedInjectConstructor(CtConstructor<? extends Object> constructor) {
-    return !constructor.getAnnotations().isEmpty()
-      && ANNOTATION_INJECT.equals(constructor.getAnnotations().get(0).getSignature());
+  private Integer getIndexAnnotatedInjectConstructor(CtConstructor<? extends Object> constructor) {
+    List<CtAnnotation<?>> annotations = constructor.getAnnotations();
+    
+    if (!annotations.isEmpty()) {
+      for (int index = 0; index < annotations.size(); index++) {
+        if (ANNOTATION_INJECT.equals(annotations.get(index).getSignature()))
+          return index;
+      }
+    }
+    return null;
   }
   
-  private void remplaceConstructor(CtConstructor<? extends Object> constructor) {
+  
+  private void remplaceConstructor(CtConstructor<? extends Object> constructor, Integer indexAnnotation) {
+    
     PrintWriter writer;
     try {
-    	writer = new PrintWriter("C:/Users/AnaGissel/Desktop/bindmap-list.txt");
-      //writer = new PrintWriter("C:/Users/Pauline/Desktop/bindmap-list.txt");
+      writer = new PrintWriter("C:/Users/Pauline/Desktop/bindmap-list.txt");
       
-      // Pour visualiser
-      writer.println("Constructor.getType.getQualifiedName : "+constructor.getType().getQualifiedName());
-      writer.println("Constructor.getType.getSimpleName    : "+constructor.getType().getSimpleName());
+      CtParameter<?> ctParamToReplace = null;
+      String nameParamToReplace = null;
+      String typeParamToReplace = null;
+      String typeSubstituteParam = null;
       
-      // Contenu de la bindsMap
-      writer.println();
-      writer.println("***** Contenu de la bindsMap *****");
-      for ( String key : SaveMap.getAllKeys()) {
-        writer.println("key : "+key);
-      }
-      writer.println("\n\n***** Fin contenu *****");
-      
-      
-      // On recupere les parametres du constructeur
-      List<String> params = SaveMap.getConstructorParameters(constructor.getType().getQualifiedName());
-      
-      writer.println("\n\n***** Les parametres du constructeur *****");
-      for(String param : params) {
-        writer.println("-> Param: "+param);
+      // Pour tous les parametres du constructeur @Inject
+      for (CtParameter<?> ctParameter : constructor.getParameters()) {
+
+        // On recupere le parametre remplacant
+        typeSubstituteParam = getSubstituteParameter(ctParameter.getType().getQualifiedName());
         
-        if(SaveMap.containsClass(param)) {
-          writer.println("-> Param remplacant : " + SaveMap.getClassValue(param));
-        } else {
-          writer.println("-> Pas de param remplacant");
+        // S'il existe un parametre remplacant alors on peut remplacer
+        if (!typeSubstituteParam.isEmpty()) {
+          ctParamToReplace = ctParameter;
+          typeParamToReplace = ctParameter.getType().getQualifiedName();
+          nameParamToReplace = ctParameter.getSimpleName();
         }
+        
       }
-      writer.println("\n\n***** Fin des parametres *****");
       
-      
+      // Si on a bien un parametre de subtitution
+      if (ctParamToReplace != null) {
+        
+        // alors on remove le parametre a remplacer
+        constructor.removeParameter(ctParamToReplace);
+        
+        // on remove l'annotation
+        constructor.removeAnnotation(constructor.getAnnotations().get(indexAnnotation));
+   
+        // remplacement de l'affectation de l'attribut a remplacer
+        for (CtStatement statement : constructor.getBody().getStatements()){
+          writer.println("statement : "+statement.toString());
+        }
+        
+        // on recupere l'attribut de la classe a remplacer (son index)
+        CtClass<?> classe = constructor.getParent(CtClass.class);
+        Integer indexField = getIndexFieldToReplace(classe.getFields(), typeParamToReplace);
+
+        // on le remplace avec le nouvel attribut
+        classe.getFields().get(indexField).replace(createNewField(getSubstituteParameter(typeParamToReplace), nameParamToReplace));
+      }
+
       writer.close();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
   }
+  
+  /**
+   * Creates a new field
+   * @param type the type of the field to create
+   * @param name the name of the field to create
+   * @return
+   */
+  private CtFieldImpl createNewField(String type, String name) {
+    CtFieldImpl newField = new CtFieldImpl();
+    
+    CtTypeReferenceImpl newref = new CtTypeReferenceImpl();
+    newref.setSimpleName(type);
+    
+    newField.setType(newref);
+    newField.setSimpleName(name);
+    newField.setVisibility(ModifierKind.PRIVATE);
+    
+    return newField;
+  }
+  
+  /**
+   * Returns the index of the field to replace in the class 
+   * @param fields the list of the fields in the class
+   * @param typeField the type's field to replace
+   * @return
+   */
+  private Integer getIndexFieldToReplace(List<CtField<?>> fields, String typeField){
+    for (int index = 0; index < fields.size(); index++) {
+      if (typeField.equals(fields.get(index).getType().getQualifiedName())) {
+        return index;
+      }
+    }
+    return null;
+  }
+  
+  /**
+   * Returns the substitute of the oldParameter if exists, an empty string if not.
+   * @param oldParameter the parameter to replace
+   * @return
+   */
+  private String getSubstituteParameter(String oldParameter) {
+    if(SaveMap.containsClass(oldParameter)) {
+      return SaveMap.getClassValue(oldParameter);
+    }
+    return "";
+  }
+  
 }
