53102946dbf4ec71cd15649b4d0e9a144ffc07aa
Pauline Dessingue <dessingue.pauline@gmail.com>
2015-11-11 17:50:55 +0100

    Problem : bindsMap empty ?

--- src/main/java/com/iagl/processor/ConstructorProcessor.java
+++ src/main/java/com/iagl/processor/ConstructorProcessor.java
@@ -7,6 +7,7 @@ import java.util.List;
 import spoon.processing.AbstractManualProcessor;
 import spoon.reflect.declaration.CtConstructor;
 import spoon.reflect.visitor.filter.TypeFilter;
+import util.SaveMap;
 
 public class ConstructorProcessor extends AbstractManualProcessor {
 
@@ -48,6 +49,44 @@ public class ConstructorProcessor extends AbstractManualProcessor {
   }
   
   private void remplaceConstructor(CtConstructor<? extends Object> constructor) {
-    
+    PrintWriter writer;
+    try {
+      writer = new PrintWriter("C:/Users/Pauline/Desktop/bindmap-list.txt");
+      
+      // Pour visualiser
+      writer.println("Constructor.getType.getQualifiedName : "+constructor.getType().getQualifiedName());
+      writer.println("Constructor.getType.getSimpleName    : "+constructor.getType().getSimpleName());
+      
+      // Contenu de la bindsMap
+      writer.println();
+      writer.println("***** Contenu de la bindsMap *****");
+      for ( String key : SaveMap.getAllKeys()) {
+        writer.println("key : "+key);
+      }
+      writer.println("\n\n***** Fin contenu *****");
+      
+      
+      
+      
+      // On recupere les parametres du constructeur
+      List<String> params = SaveMap.getConstructorParameters(constructor.getType().getQualifiedName());
+      
+      writer.println("\n\n***** Les parametres du constructeur *****");
+      for(String param : params) {
+        writer.println("-> Param: "+param);
+        
+        if(SaveMap.containsClass(param)) {
+          writer.println("-> Param remplacant : " + SaveMap.getClassValue(param));
+        } else {
+          writer.println("-> Pas de param remplacant");
+        }
+      }
+      writer.println("\n\n***** Fin des parametres *****");
+      
+      
+      writer.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 }
