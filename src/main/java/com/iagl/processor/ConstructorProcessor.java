8b3eac6df7823d56bc97b926fcb13b854c1e8336
Pauline Dessingue <dessingue.pauline@gmail.com>
2015-11-14 14:04:51 +0100

    Remove Inject annotations on elements (constructors+fields)

--- src/main/java/com/iagl/processor/ConstructorProcessor.java
+++ src/main/java/com/iagl/processor/ConstructorProcessor.java
@@ -6,6 +6,7 @@ import spoon.processing.AbstractManualProcessor;
 import spoon.reflect.declaration.CtAnnotation;
 import spoon.reflect.declaration.CtConstructor;
 import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtField;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 public class ConstructorProcessor extends AbstractManualProcessor {
@@ -29,13 +30,7 @@ public class ConstructorProcessor extends AbstractManualProcessor {
     List<CtConstructor> allConstructors = getFactory().Package().getRootPackage().getElements(new TypeFilter(CtConstructor.class));
 
     for (CtConstructor constructor : allConstructors) {
-      Integer indexInjectAnnotation = getIndexInjectAnnotation(constructor);
-
-      // Si le constructeur est annote par @Inject, on supprime l'annotation
-      if (indexInjectAnnotation != null) {
-        CtAnnotation<?> annotationInject = constructor.getAnnotations().get(indexInjectAnnotation);
-        constructor.removeAnnotation(annotationInject);
-      }
+      removeInjectAnnotation(constructor);
     }
   }
 
@@ -43,26 +38,25 @@ public class ConstructorProcessor extends AbstractManualProcessor {
    * Transforms the annotated class fields 
    */
   public void transformAnnotatedFields() {
+    // Recherche des champs annotes
+    List<CtField> allFields = getFactory().Package().getRootPackage().getElements(new TypeFilter(CtField.class));
 
+    for (CtField field : allFields) {
+      removeInjectAnnotation(field);
+    }
   }
 
   /* ************************************* UTILS **********************************/
 
   /**
-   * Returns the index of the Inject annotation of the element
-   * @param element the element to verify
-   * @return the index of annotation if exist, null if not
+   * Remove the inject annotation on the element if exists, do nothing if not
+   * @param element the CtElement to verify
    */
-  private Integer getIndexInjectAnnotation(CtElement element) {
-    List<CtAnnotation<?>> annotations = element.getAnnotations();
-
-    if (!annotations.isEmpty()) {
-      for (int index = 0; index < annotations.size(); index++) {
-        if (ANNOTATION_INJECT.equals(annotations.get(index).getSignature()))
-          return index;
-      }
+  private void removeInjectAnnotation(CtElement element) {
+    for (CtAnnotation annotation : element.getAnnotations()) {
+      if (ANNOTATION_INJECT.equals(annotation.getSignature()))
+        element.removeAnnotation(annotation);
     }
-    return null;
   }
-  
+
 }
